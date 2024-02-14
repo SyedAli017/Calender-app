b0e0b059b56ed18a8343a9971e2448799829738b
Syed Ali <sydali3103@gmail.com>
2024-02-14 16:01:47 +0530

    added calnder thene abd marked dates

--- modals/CalendarModal.js
+++ modals/CalendarModal.js
@@ -1,9 +1,22 @@
+import React, { useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import Calendar from "react-native-calendars/src/calendar";
 
 const CalendarModal = ({ toggleModal }) => {
+  const [markedDates, setMarkedDates] = useState({});
+
   const handleDayPress = (day) => {
     console.log("Pressed date:", day.dateString);
+    setMarkedDates({
+      ...markedDates,
+      [day.dateString]: {
+        selected: true,
+        disableTouchEvent: true,
+        selectedColor: "#bf696f",
+        selectedTextColor: "white",
+      },
+    });
+
     toggleModal();
   };
 
@@ -13,6 +26,13 @@ const CalendarModal = ({ toggleModal }) => {
         style={styles.calendar}
         hideExtraDays
         onDayPress={handleDayPress}
+        theme={{
+          todayTextColor: "white",
+          todayBackgroundColor: "#55A0EE",
+          arrowColor: "#55A0EE",
+          textSectionTitleColor: "#55A0EE",
+        }}
+        markedDates={markedDates}
       />
     </View>
   );
